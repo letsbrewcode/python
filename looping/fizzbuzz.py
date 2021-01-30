43557b9a9e829024c808760fc603e89d96fc6d83
Himanshu Sharma <letsbrewcode@gmail.com>
2021-01-30 13:48:21 -0800

    Add fizzbuzz.py

---
+++ looping/fizzbuzz.py
@@ -0,0 +1,20 @@
+# Problem: FizzBuzz
+
+# Complete the function below. For a number N, print all the numbers starting
+# from 1 to N, except
+# 1. For numbers divisible by 3, print "Fizz"
+# 2. For numbers divisible by 5, print "Buzz"
+# 3. For numbers divisible by both 3 and 5, print "FizzBuzz"
+
+# Example:
+# Input: N = 20
+# Output: 
+# 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, 
+# FizzBuzz, 16, 17, Fizz, 19, Buzz
+
+def fizzbuzz(n):
+    return
+
+if __name__ == "__main__":
+    fizzbuzz(100)
+
