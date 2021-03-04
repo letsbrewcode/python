68d75cc1d62cae9f0e0de7680a0a32689dc56659
Himanshu Sharma <letsbrewcode@gmail.com>
2021-03-03 18:07:09 -0800

    Create sort_number.py

---
+++ files/sort_number.py
@@ -0,0 +1,10 @@
+# Write a function to sort all the numbers present in a file and write it back to
+# another file. The names of input and output files should be provided to function
+# from the caller.
+
+def sort_numbers(input_file, output_file):
+	pass
+
+
+if __name__ == '__main__':
+	sort_numbers('numbers.txt', 'sorted_numbers.txt')
